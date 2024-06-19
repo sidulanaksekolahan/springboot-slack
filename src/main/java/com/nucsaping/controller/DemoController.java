b395ea6011fc7c20e55704aa5cf0bd5376ed44c8
sidulanaksekolahan <mirfan.duri@gmail.com>
2024-06-19 16:36:37 +0700

    Testing pull request!

--- src/main/java/com/nucsaping/controller/DemoController.java
+++ src/main/java/com/nucsaping/controller/DemoController.java
@@ -13,8 +13,6 @@ public class DemoController {
 
     @GetMapping
     public String hello() {
-        return "Hello there, the time is " + LocalDateTime.now()
-                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:MM:SS"))
-                + ". This project uses github action!!!";
+        return "Test!!!";
     }
 }
